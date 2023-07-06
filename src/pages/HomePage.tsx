3264b0b49014b993c13e9ece78b97fd9681d0168
Rosemberg <rosembergjll@gmail.com>
2023-07-06 01:37:23 -0400

    rutas modificadas

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,11 +2,7 @@ export const HomePage = () => {
   return (
     <header id="home-page" className="header">
       <div className="brand-info">
-        <img
-          src="/public/coinsertel.jpeg"
-          alt="coinsertel logo"
-          className="logo"
-        />
+        <img src="coinsertel.jpeg" alt="coinsertel logo" className="logo" />
         <h1 className="brand-info__title">Coinsertel</h1>
         <p className="brand-info__text">Tu punto de enlace</p>
       </div>
